274774562ef1a75932bcd50cf1280cc996abae13
AfrazAhmad <afraz.ahmad@cognitiveaxis.com>
2017-08-20 04:28:52 +0500

    Datatables Sample project

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,58 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+
+        <!-- Styles -->
+    
+        {{-- <link rel="stylesheet" href="https://cdn.datatables.net/buttons/1.0.3/css/buttons.dataTables.min.css"> --}}
+        <link rel="stylesheet" type="text/css" href="{{ asset('css/bootstrap.min.css') }}">
+        <link rel="stylesheet" type="text/css" href="{{ asset('css/dataTables.bootstrap.min.css') }}">
+
+    </head>
+    <body>
+        <div class="flex-center position-ref full-height" style="padding: 30px;">
+
+            <div class="content well">
+
+                <table class="table table-responsive table-bordered display " id="myTable">
+                    <caption>Users Listing</caption>
+                    <thead>
+                        <tr>
+                            <th>Name</th>
+                            <th>Email</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach($users as $user)
+                            <tr>
+                                <td>{{ $user->name }}</td>
+                                <td>{{ $user->email }}</td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+                        
+            </div>
+        </div>
+    <script src="{{ asset('js/jquery-3.2.1.min.js') }}"></script>
+    <script src="{{ asset('js/jquery.dataTables.min.js') }}"></script>
+    <script src="{{ asset('js/dataTables.bootstrap.min.js') }}"></script>
+    <script src="{{ asset('js/dataTables.bootstrap.min.js') }}"></script>
+    <script>
+    $(document).ready(function(){
+        $('#myTable').DataTable({ "pagingType": "full_numbers" });
+
+    });
+    </script>
+
+    </body>
+
+</html>
